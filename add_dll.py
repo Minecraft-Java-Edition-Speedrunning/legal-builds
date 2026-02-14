4dc295eb4b21bf3464df8d6bf15bf926b4132593
tildejustin <tildejustin@gmail.com>
2026-02-14 02:21:44 -0500

    add version field

--- add_dll.py
+++ add_dll.py
@@ -11,17 +11,20 @@ with open(output, "r") as f:
     fields = next(reader)
     curr = list(reader)
 
+assert len(sys.argv) == 3
+filename = sys.argv[1]
+version = sys.argv[2]
+file = pathlib.Path(filename)
+if not file.exists():
+    print(f"{file}: file does not exist!", file=sys.stderr)
+    exit(1)
 
-for filename in sys.argv[1:]:
-    file = pathlib.Path(filename)
-    if not file.exists():
-        print(f"{file}: file does not exist!", file=sys.stderr)
-        continue
-    with open(file, "rb") as f:
-        name = file.name
-        checksum = hashlib.sha512(f.read()).hexdigest()
-        print(f"adding {name}: {checksum}")
-        curr.append([name, checksum])
+
+with open(file, "rb") as f:
+    name = file.name
+    checksum = hashlib.sha512(f.read()).hexdigest()
+    print(f"adding {name}: {checksum}")
+    curr.append([name, version, checksum])
 
 
 with open(output, "w", newline="") as f:
